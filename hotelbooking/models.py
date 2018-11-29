a34dd6f005d1c50aade92244bc6f9da7a2a4354a
AKBARMOHAMMAD <43230334+AKBARMOHAMMAD@users.noreply.github.com>
2018-11-29 14:39:49 +0530

    Add files via upload

--- hotelbooking/models.py
+++ hotelbooking/models.py
@@ -1,8 +1,33 @@
 from django.db import models
 import datetime
 # Create your models here.
+#admin model
+class Hotels(models.Model):
+    name=models.CharField(max_length=50)
+    address=models.CharField(max_length=250)
+    city=models.CharField(max_length=50)
+    country=models.CharField(max_length=20)
+    contact_no=models.IntegerField()
+#===================================
+class Room(models.Model):
+    hotel=models.ForeignKey(Hotels,on_delete=models.CASCADE)
+    room_type=models.CharField(max_length=50)
+    max_numbers=models.IntegerField()
+    bed_option=models.CharField(max_length=50)
+    price=models.IntegerField()
+    total_rooms=models.CharField(max_length=50)
+    date=models.DateField()
+ #=============================================
+class Review(models.Model):
+     #user=models.ForeignKey(User,on_delete=models.CASCADE)
+     hotel=models.ForeignKey(Hotels,on_delete=models.CASCADE)
+     comment=models.CharField(max_length=255)
+     rating=models.IntegerField()
+     created_at=models.DateField()
+     updated_at=models.DateField()
+#====================================================
 
-#User Model
+'''User Model
 class Register(models.Model):
     name=models.CharField(max_length=50)
     email_id=models.EmailField(max_length=20,primary_key=True)
@@ -10,12 +35,13 @@ class Register(models.Model):
     contact_no=models.IntegerField()
     address=models.CharField(max_length=100)
 class Check_Availability(models.Model):
-    room_no=models.IntegerField()
+    room_type=models.IntegerField()
+    maxno=models.CharField(max_length=10)
     check_In=models.DateField()
     check_Out=models.DateField()
 class Reserve_Room(models.Model):
-    room_no=models.ForeignKey(Check_Availability,on_delete=models.CASCADE)
-    room_type=models.CharField(max_length=50,primary_key=True)
+    room_no=models.IntegerField(max_length=50,primary_key=True)
+    room_type=models.ForeignKey(Check_Availability,on_delete=models.CASCADE)
     credit_card=models.IntegerField()
     debit_card=models.IntegerField()
     check_In=models.DateField()
@@ -31,6 +57,6 @@ class Display(models.Model):
 class Cancle_Room(models.Model):
     user_name=models.CharField(max_length=50)
     room_no=models.ForeignKey(Display,on_delete=models.CASCADE)
-    cust_id=models.IntegerField()
+    cust_id=models.IntegerField()'''
 
 
